1086dde1afa8958cfe253594484211d2a1091971
Declan Taylor <declan.taylor@pcs-instruments.com>
2020-07-17 17:23:00 +0100

    Fixes issues with duplicates

--- source/Demos/CachedPathSuggestBoxDemo/Infrastructure/LiteRepository.cs
+++ source/Demos/CachedPathSuggestBoxDemo/Infrastructure/LiteRepository.cs
@@ -7,45 +7,72 @@ using LiteDB;
 
 namespace CachedPathSuggestBoxDemo.Infrastructure
 {
-    public sealed class LiteRepository
-    {
-        private const string DbPath = @"..\..\..\Data\KeyValue.litedb";
+	/// <summary>
+	/// Implements a static service provider for searching and editing a LiteDB file.
+	/// </summary>
+	public sealed class LiteRepository
+	{
+		#region fields
+		private const string DbPath = @"..\..\..\Data\KeyValue.litedb";
 
-        // Collection needs a name because object, keyvaluepair<string,string> is generic.
-        private const string CollectionName = "collection";
+		// Collection needs a name because object, keyvaluepair<string,string> is generic.
+		private const string CollectionName = "collection";
+		#endregion fields
 
-        // Explicit static constructor to tell C# compiler
-        // not to mark type as beforefieldinit
-        static LiteRepository()
-        {
-        }
+		#region ctors
+		/// <summary>
+		/// Explicit static constructor to tell C# compiler
+		/// not to mark type as beforefieldinit
+		/// </summary>
+		static LiteRepository()
+		{
+		}
 
-        public void Insert(string k, string? collectionName = null)
-        {
-            using var db = new LiteDatabase(DbPath);
-            var col = db.GetCollection<KeyValuePair<string, DateTime>>(collectionName ?? CollectionName);
-            col.Upsert(new KeyValuePair<string, DateTime>(k, DateTime.Now));
-        }
+		/// <summary>
+		/// Class constructor
+		/// </summary>
+		private LiteRepository()
+		{
+			Directory.GetParent(DbPath).Create();
 
-        public IEnumerable<KeyValuePair<string, DateTime>> Filter(string key, string? collectionName = null)
-        {
-            using var db = new LiteDatabase(DbPath);
-            var col = db.GetCollection<KeyValuePair<string, DateTime>>(collectionName ?? CollectionName);
-            return string.IsNullOrWhiteSpace(key)?
-                col.Query().ToArray():
-                col.Find(Query.Contains(nameof(KeyValuePair<string, DateTime>.Key), key)).ToArray();
-        }
+			// Id needed to ensure Upsert works.
+			BsonMapper.Global.Entity<KeyValuePair<string, DateTime>>().Id(x => x.Key);
+		}
+		#endregion ctors
 
-        private LiteRepository()
-        {
-            Directory.GetParent(DbPath).Create();
+		/// <summary>
+		/// Gets the repository instance from this static instance.
+		/// </summary>
+		public static LiteRepository Instance { get; } = new LiteRepository();
 
-            // Id needed to ensure Upsert works.
-            BsonMapper.Global.Entity<KeyValuePair<string, string>>()
-                .Id(x => x.Value);
+		/// <summary>
+		/// Inserts a new string into the collection of bookmarked strings.
+		/// </summary>
+		/// <param name="k"></param>
+		/// <param name="collectionName"></param>
+		public void Insert(string k, string? collectionName = null)
+		{
+			using var db = new LiteDatabase(DbPath);
+			var col = db.GetCollection<KeyValuePair<string, DateTime>>(collectionName ?? CollectionName);
 
-        }
+			// Make sure string is not already present
+			col.Upsert(new KeyValuePair<string, DateTime>(k, DateTime.Now));
+		}
 
-        public static LiteRepository Instance { get; } = new LiteRepository();
-    }
+		/// <summary>
+		/// Filters the collection of bookmark strings by the given string and
+		/// returns the resulting collection.
+		/// </summary>
+		/// <param name="key"></param>
+		/// <param name="collectionName"></param>
+		/// <returns></returns>
+		public IEnumerable<KeyValuePair<string, DateTime>> Filter(string key, string? collectionName = null)
+		{
+			using var db = new LiteDatabase(DbPath);
+			var col = db.GetCollection<KeyValuePair<string, DateTime>>(collectionName ?? CollectionName);
+			return string.IsNullOrWhiteSpace(key) ?
+				col.Query().ToArray() :
+				col.Find(Query.Contains(nameof(KeyValuePair<string, DateTime>.Key), key)).ToArray();
+		}
+	}
 }
